e5a82e908bcfef736c621930e65d39724ea17952
Jason <hwanth85@gmail.com>
2016-06-17 17:50:05 +0900

    리액트 컴포넌트 조합하기

--- app/App.js
+++ app/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 
-class App extends Component {
-  render(){
+// class App extends Component {
+//   render(){
+//     var place = "World"
+//     return (
+//       <h1>Hello {place}!!</h1>
+//     );
+//   }
+// }
+
+// 부모 컴포넌트
+class GroceryList extends Component {
+  render() {
     return (
-      <h1>Hello World</h1>
-    );
+        <ul>
+          <ListItem quantity="1" name="Bread" />
+          <ListItem quantity="6" name="Eggs" />
+          <ListItem quantity="2" name="Milk" />
+        </ul>
+    )
   }
 }
 
-render(<App />, document.getElementById('root'));
+// 자식 컴포넌트
+class ListItem extends Component {
+    render() {
+        return (
+            <li>
+                {this.props.quantity} x {this.props.name}
+            </li>
+        )
+    }
+}
+
+render(<GroceryList />, document.getElementById('root'));

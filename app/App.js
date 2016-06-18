879bdcf2cdfe58f0d638af1f42d4298c9e23d96e
Jason <hwanth85@gmail.com>
2016-06-18 16:48:10 +0900

    KanbanBoard 1단계 완성.

--- app/App.js
+++ app/App.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 
-// class App extends Component {
-//   render(){
-//     var place = "World"
-//     return (
-//       <h1>Hello {place}!!</h1>
-//     );
-//   }
-// }
+import KanbanBoard from './KanbanBoard';
 
-// 부모 컴포넌트
-class GroceryList extends Component {
-  render() {
-    return (
-        <ul>
-          <ListItem quantity="1" name="Bread" />
-          <ListItem quantity="6" name="Eggs" />
-          <ListItem quantity="2" name="Milk" />
-        </ul>
-    )
-  }
-}
-
-// 자식 컴포넌트
-class ListItem extends Component {
-    render() {
-        return (
-            <li>
-                {this.props.quantity} x {this.props.name}
-            </li>
-        )
+let cardsList = [
+    {
+        id: 1,
+        title: "Read the Book",
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: "Code along with the samples in the book",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Read the Book",
+        status: "done",
+        tasks: []
     }
-}
+];
 
-render(<GroceryList />, document.getElementById('root'));
+render(<KanbanBoard cards={cardsList}/>, document.getElementById('root'));

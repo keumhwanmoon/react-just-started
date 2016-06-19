98a3e6c8648ef715b77b371b1ab7a1012a612e11
Jason <hwanth85@gmail.com>
2016-06-19 17:27:26 +0900

    토글 액션 추가.

--- app/CheckList.js
+++ app/CheckList.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class CheckList extends Component {
     render() {
-        let tasks = this.props.task.map((task) => (
+        let tasks = this.props.tasks.map((task) => (
             <li key={task.id} className="checklist_task">
                 <input type="checkbox" defaultChecked={task.done}/>
                 {task.name}
